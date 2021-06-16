830348e369e0be9d9290026bb2e285bfc45372cb
Alex Overbeck <alex@expectedbehavior.com>
2021-06-16 14:12:09 -0400

    Cleans up overall UI design
    Why is this change needed?
    --------------------------
    The Pokedex is now functional, but the UI could use some cleanup. Let's
    make sure the typography looks nice and make sure that it works on
    mobile devices.
    
    How does it address the issue?
    ------------------------------
    This swaps out the pixelated font for Roboto to make it feel more
    Material Design inspired. The styles for the top of the pokedex have
    also been tweaked to provide a better experience on various screen
    sizes.

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,7 +7,9 @@ function Details(props){
     <div className="details">
       <h1 className="details__title">{props.pokemon.name}</h1>
       <h2 className="details__subtitle">#{props.pokemon.id}</h2>
-      <p>{description}</p>
+      <div className="details__description">
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
